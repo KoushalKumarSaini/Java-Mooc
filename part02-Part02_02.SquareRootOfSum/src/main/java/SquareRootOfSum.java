87075e3f2a627a063faba63c629a3923a6433c33
Koushal Saini <koushalsaini54@gmail.com>
2025-06-24 23:01:09 +0530

    Updated 4 exercises from Part 2 after completing them

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,10 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the two numbers:");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(Math.sqrt(a+b));
 
     }
 }
--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -1,10 +1,16 @@
-
 import java.util.Scanner;
 
-public class AbsoluteValue {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+public class AbsoluteValue{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give the number:");
+        int n = sc.nextInt();
+        
+        if(n<0){
+            System.out.println(n*-1);
+        } else{
+            System.out.println(n);
+        }
+        
     }
 }
