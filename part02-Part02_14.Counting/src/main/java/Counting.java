d13ee6ae37a3954a09363a040a0cc7b94975e78a
Koushal Saini <koushalsaini54@gmail.com>
2025-06-26 21:25:57 +0530

    Part 2 - Sub-part 3 started: Completed Counting exercise

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -5,6 +5,11 @@ public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        for(int i = 0; i<=n; i++){
+            System.out.println(i);
+        }
 
     }
 }
